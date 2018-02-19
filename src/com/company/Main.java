1716a1c49e2531cc64f64f0e2b369b4a2a368b03
RedBomb1811 <dimadudko00@gmail.com>
2018-02-19 16:57:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+//TODO Checked TODO comment
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println( "Hello World!!!");
+
+    }
+}
+//TODO TODO№2
